f94606ba010f9f88fff12c226eac8afc5ead370f
Jeff Malnor <jcmalnor@gmail.com>
2016-10-25 15:54:52 -0400

    first commit

---
+++ src/Variables.java
@@ -0,0 +1,31 @@
+// for git practice
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		int a = 63;
+		int b = 45;
+		
+		System.out.println("a is " + a);
+		System.out.println("b is " + b);
+		
+		a = a+10;
+		a += 10;
+		
+		a = a + 1; //binary operators
+		a += 1; //extended assignment operator
+		a++; //unary operator
+		
+		System.out.println("a is " + a);
+		
+		double c = 3.5;
+		int d = (int)c;
+		long e = Math.round(c);
+		System.out.println("c is " +c);
+		System.out.println("d is " +d);
+		System.out.println("e is " +e);
+		
+	}
+
+}
